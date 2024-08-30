4a88814894ca4bc152241dc00218fab35c4f1d63
alexokonski <alexanderokonski@gmail.com>
2024-08-30 14:35:21 -0700

    No code, bevy_simple_networking copied and 'ported' to bevy 0.14

---
+++ src/networking/events.rs
@@ -0,0 +1,19 @@
+use std::{io, net::SocketAddr};
+
+use bytes::Bytes;
+
+use super::message::Message;
+
+#[derive(bevy::prelude::Event)]
+pub enum NetworkEvent {
+    // A message was received from a client
+    Message(SocketAddr, Bytes),
+    // A new client has connected to us
+    Connected(SocketAddr),
+    // A client has disconnected from us
+    Disconnected(SocketAddr),
+    // An error occurred while receiving a message
+    RecvError(io::Error),
+    // An error occurred while sending a message
+    SendError(io::Error, Message),
+}
